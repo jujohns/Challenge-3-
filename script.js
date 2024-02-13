c4b14f97e317a8c366b6b1f5b5074d9ea7647f45
jujohns <jjohnskfw@gmail.com>
2024-02-12 18:07:53 -0600

    homework

---
+++ script.js
@@ -0,0 +1,46 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Function to generate a random password
+function generatePassword() {
+  // Define the character sets for the password
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-_=+[]{}|;:'\",.<>/?";
+
+  // Combine all character sets
+  var allChars = lowercaseChars + uppercaseChars + numericChars + specialChars;
+
+  // Set the desired password length
+  var passwordLength = 12;
+
+  // Generate the random password
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
